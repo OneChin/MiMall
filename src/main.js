4f4968bac1e5c44d523041ca3aa6ab869a608cb7
OneChin <864336723@qq.com>
2020-11-01 10:55:52 +0800

    接口拦截处理

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 
+
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+// 设置超时，必须
+
+
+axios.defaults.timeout = 8000;
+// 拦截器
+axios.interceptors.response.use(function(response) {
+  // 接口的值
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+    // 错误拦截
+  } else if (res.status == 10 ) {
+    window.location.href = '/#/login';
+  } else {
+    alert(res.msg);
+  }
+
+})
+// 挂载实例
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
